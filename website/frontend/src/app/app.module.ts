a7ba2d989c5ab3a2e15a9b95b7490cfeeba2d4c9
dlindsay93 <davidlindsay874@gmail.com>
2019-01-16 16:59:23 -0600

    have upload button working. Cannot find pathway to upload file/CORS error.

--- website/frontend/src/app/app.module.ts
+++ website/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AssetsComponent } from './assets/assets.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,14 @@ import { FooterComponent } from './footer/footer.component';
     PortfolioComponent,
     ContactComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AssetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- website/frontend/src/app/portfolio/portfolio.component.ts
+++ website/frontend/src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    HttpClient,
+    HttpClientModule
+  ],
+  providers: [
+    HttpClientModule,
+    HttpClient,
+  ],
+})
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
-})
+  styleUrls: ['./portfolio.component.css'],
+  })
+
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+  selectedFile: File = null;
 
+  onFileSelected(event){
+    this.selectedFile = <File> event.target.files[0];
+    console.log(event);
+  }
+  // @CrossOrigin
+  onUpload(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post("../assets", fd)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 }
